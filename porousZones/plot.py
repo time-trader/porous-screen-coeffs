6e2cc82356640ef2b9809d2ef35cb4773a798d3f
Juri <yuriy.marykovskiy@mail.polimi.it>
2022-07-22 11:51:41 +0200

    REF:Remove unused plotting style settings

--- porousZones/plot.py
+++ porousZones/plot.py
@@ -1,55 +1,49 @@
+import os.path
+
 import plotly.graph_objects as go
 import pandas as pd
 
-def plot_forces(title, forces_dict):
+
+def plot_forces(title, forces_dict, save_eps=False):
     fig = go.Figure()
 
-    color_list = [
-        "rgb(0, 0, 0)",
-        "rgb(120, 120, 120)",
-        "rgb(180, 180, 180)"
-    ]
-
-    marker_list = [3, 4, 5, 6]
-
-    marker_i=0
-    color_i=0
 
     for key in forces_dict.keys():
         if "Analytical" in key:
-            line_dict = dict(color=color_list[color_i])
             _mode = 'lines'
-            color_i+=1
         else:
-            line_dict = dict()
             _mode = 'markers'
-            marker_i+=1
 
         fig.add_trace(go.Scatter(x=forces_dict[key][0], y=forces_dict[key][1],
                                  mode=_mode, name=key,
-                                 line=line_dict,
                                  marker=dict(
-                                    symbol = marker_list[marker_i],
-                                    size=15,
-                                    color="black"
+                                    # size=5,
                                  ))
                       )
 
     fig.update_layout(
         title={
-            'text': title,
+            #'text': title,
             'y':0.95,
             'x':0.5,
             'xanchor': 'center',
             'yanchor': 'top'},
+        # xaxis=dict(range=[-90, 90]),
         xaxis_title="AoA [deg]",
         yaxis_title="Forces [N]",
         legend=dict(
+            orientation="h",
             yanchor="top",
-            y=0.99,
-            xanchor="right",
-            x=0.99),
-        template='plotly_white'
-
+            y=1.25,
+            xanchor="left",
+            x=0.05),
+        template='plotly_white',
+        font_family="Computer Modern"
     )
-    fig.show()
+
+    # fig.update_xaxes(dtick=30)
+
+    if save_eps:
+        fig.write_image(os.path.join("plots", "fig_{}.eps".format(title)), width=600, height=400, scale=20)
+    else:
+        fig.show()
